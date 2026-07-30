a0034c25dda7cc33aee5122394bcb2798a0b802d
daniel <danielsalazarvargas953@gmail.com>
2026-07-30 16:19:16 -0500

    feat/ The seed data was processed, taking the security module into account.

--- 01_ddl/03_tables/006_security_module/001_create_table_roles.sql
+++ 01_ddl/03_tables/006_security_module/001_create_table_roles.sql
@@ -2,5 +2,6 @@ CREATE TABLE security.roles (
     role_id      SERIAL PRIMARY KEY,
     name         VARCHAR(50) NOT NULL UNIQUE,
     description  VARCHAR(255),
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT now()
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NULL
 );
